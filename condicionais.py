9635b4d560f68df328d9bf8d082f83f2ce9eb2e9
alm1rdev <silvaalmir556@gmail.com>
2025-08-14 20:33:53 -0300

    condicionai

---
+++ condicionais.py
@@ -0,0 +1,26 @@
+# if | elif | else
+# se | se nao se| se nao
+
+
+# operadores de comparacao
+ 
+ #< ( 3<4 = true)
+ #>( 4 >3 = true)
+ #==  (2==2 =true)
+#!= (2!=1 =true)
+#<= ( 3 <=3 = true)
+#>= (4>=4 true)
+
+num_1= float(input("digite o primeiro numero:"))
+num_2=float(input("digite o segundo numero:"))
+
+if num_1 > num_2 :
+  print(f"o {num_1} e maior {num_2}")
+
+elif num_1< num_2:
+  print(f'o {num_2} e maior que {num_1}')
+else:
+  print(f'o {num_1} e igual {num_2}')
+
+
+
