5d4a938504cfe852c24eeaa5207ea5d3e4e6cb4f
Artur de Carvalho <arturplacido13@gmail.com>
2025-08-21 20:26:23 -0300

    ola,mundo

---
+++ exercicis operadores logicos.py
@@ -0,0 +1,10 @@
+nome='almir'
+curso='python'
+
+digite_nome=input('digite seu nome :')
+digite_curso=input('digite se curso:')
+
+if digite_nome==nome and digite_curso==curso:
+    print('bem-vindo a turma 29!')
+else:
+    print('voce nao esta matriculado!')
