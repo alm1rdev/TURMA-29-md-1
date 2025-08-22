ab4d9fa25ec7ef5a70e3f09d59f4e36aa006bfd0
Artur de Carvalho <arturplacido13@gmail.com>
2025-08-21 21:31:13 -0300

    or operadores

--- OR operador.py
+++ OR operador.py
@@ -2,12 +2,16 @@ nome_usuario='almir'
 email= 'almir@exemplo.com'
 senha=12345
 
+
+#per
 usuario=input('digite seu usuario:')
 deemail=input(' digite seu email:')
 senha_rece=int(input('digite sua senah:'))
 
+
+#condiçoes
 if not senha_rece:
     print('nao digitou a senha')
 elif(usuario==nome_usuario or deemail==email) and senha==senha_rece:
     print('digite sua senha!')
-else:'digite seu usuario e email corretamente'
+else:'digite seu usuario ou email corretamente'
--- exercicis operadores logicos.py
+++ exercicis operadores logicos.py
@@ -1,9 +1,15 @@
+#exercicio operador- verificaçao curso
+
+#variaveis
 nome='almir'
 curso='python'
 
-digite_nome=input('digite seu nome :')
-digite_curso=input('digite se curso:')
 
+#perguntas
+digite_nome=input('digite seu nome :')
+digite_curso=input('digite seu curso:')
+
+# condiçoes
 if digite_nome==nome and digite_curso==curso:
     print('bem-vindo a turma 29!')
 else:
