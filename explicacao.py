83d1f4a27d7f0fc4b6b09e40cd600e4f4dbd37fb
Artur de Carvalho <arturplacido13@gmail.com>
2025-08-26 20:43:58 -0300

    explicacao

---
+++ explicacao.py
@@ -0,0 +1,27 @@
+# 0 1 2 3 4 56 789
+# t u r m a 29 softex 
+#                   -3-2-1
+
+
+# [c:f:i]
+#comeco
+#fim
+#intervalo
+
+#in, not in == procura se tem um caractere expecifica na string
+
+curso='turma 29 softex'
+print('29'in curso)
+#o print vai mostrar se tem o 29 na variavel curso, caso NAO tenha é imprimida na tela um FALSE
+
+
+
+
+# len() == serve para retornar o NUMERO de CARACTERES de uma sequência de texto, como uma palavra, frase ou número
+# .lower() == deixa tudo minusculo
+# .upper()== deixa tudo maiusculo
+
+curso2= 'Turma 29 Softex'
+tudo_minusculo=curso.lower()
+print(tudo_minusculo)
+# o print vai imprimir tudo da variavel em minusculo
