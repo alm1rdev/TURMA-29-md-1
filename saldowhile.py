2122ddd1ea44a4b2cd47a313e66db6b6461fed27
Artur de Carvalho <arturplacido13@gmail.com>
2025-09-02 20:35:43 -0300

    atu

---
+++ saldowhile.py
@@ -0,0 +1,8 @@
+# aumentar saldo bancario
+
+saldo_inicial= 98
+
+while saldo_inicial<200:
+    
+    print(f'seu saldo e de {saldo_inicial}')
+    saldo_inicial +=10
---
+++ tabuada.py
@@ -0,0 +1,18 @@
+# tabuada
+
+#variavel.isdigit() verifica se foi digitado um numero ex:
+
+
+contador=0
+
+
+
+numero=int(input("digite qual o numero da tabuada :") )
+while contador<=10:
+    tabuada= numero* contador
+    print(f' {numero} x {contador} = {tabuada}')
+
+    contador+=1
+if numero.isdigit():
+    print('digite um numero')
+
