4452ee80fb1c3cc1943163581c10aa924298a7cf
alm1rdev <silvaalmir556@gmail.com>
2025-09-03 20:20:48 -0300

    atu

--- exercicioBRAKE_continue.py
+++ exercicioBRAKE_continue.py
@@ -1,20 +1,36 @@
 #caçculadora  loop
 
 while True:
-    numero_1=(input('digite um numero ou digite sair para sair')).isdigit()
-    operador= input('qual tipo de operacao:')
-    numero_2=     (input('digite outro numero:'))
+    numero_1=input('digite um numero ou digite sair para encerar : ')
+    if numero_1.lower()=='sair':
+        print('calculadora encerrada')
+        break
+    operador=input('digite tipo de operacao ou SAIR:')
+    numero_2=input('digite outro numero SAIR : ')
 
-    if (numero_1  and numero_2).isdigit() and operador=='+':
-        print(numero_1 + numero_2)
- elif (numero_1  and numero_2).isdigit() and operador =='-'
-        print(numero_1-numero_2)
-    elif operador=='/':
-        print(numero_1/numero_2)
-    elif operador=='*':
-     print(numero_1*numero_2)
-     continue
+    if operador .lower()=='sair':
+        print('calculadora encerrada')
+    elif numero_2.lower()=='sair':
+        print('calculaora encerrada')
+    if numero_1.isdigit() and numero_2.isdigit():
+        numero_1= float(numero_1)
+        numero_2= float(numero_2)
 
-    elif(  numero_1 or numero_2 or operador).lower()=='sair':
-       print('voce quis parar')
-       break
+        if operador =='+':
+            print(numero_1+numero_2)
+        elif operador == '-':
+            print(numero_1-numero_2)
+        elif operador =='/':
+            if numero_2!=0:
+             print(numero_1 / numero_2 )
+            else:
+                print('erro ,digite um operador valido')
+                continue
+        elif operador=='*':
+            print(numero_1* numero_2)
+        else: 
+            print('ERRO digite um operador valido')
+            continue
+    else:
+        print('digite numeros validos')
+        continue
