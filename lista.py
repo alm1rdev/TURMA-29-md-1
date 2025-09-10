63247926d9734879a37d94867c9267a7d04b16cd
Artur de Carvalho <arturplacido13@gmail.com>
2025-09-10 01:06:58 -0300

    atu

---
+++ lista.py
@@ -0,0 +1,24 @@
+#variavel= ' pedro'
+ #         0     1   2     3  4  5     6   7
+#texto= ['turma',True,bool,10,20,float,[],'almir']
+
+#texto2=[2,3]
+
+#texto.append('aluno novo')
+
+#texto.insert(5,'mais')
+
+#texto.pop(3) # pop remove mas pod eser recuperado 
+
+#del(texto[4])
+
+#texto.clear() # limpa toda a lista
+
+#texto.extend(texto2)
+
+
+
+lista= ['natan','matheus','samuel','hudson','thais','antonio','larissa','maria fernanda','josivanio','carlos','rielson','regivan','almir']
+
+for i in range(len(lista)):
+    print(f'{i+1}   {lista[i]}')
---
+++ try_ except.py
@@ -0,0 +1,5 @@
+try:
+    numero=int(input('digite um numero'))
+    print(f'o numero digitdo foi {numero}')
+
+except:
